610e5c228df6a6b45a32038aec4b46ef6ce4b5c4
AntonioShi <694482337@qq.com>
2019-04-20 19:22:08 +0800

    photos

--- config.js
+++ config.js
@@ -1,25 +1,26 @@
 module.exports = {
 	cdnDomain: '/',
-	title: 'Zing Gallery',
+	title: 'star dust Gallery',
 	avatar: './img/avatar.png',
 	wording: {
-		noAccess: '抱歉，你没有权限访问'
+		 noAccess: '抱歉，你没有权限访问'
 	},
 	albums: {
-		"青海湖": {
-			thumbnail: "打马而过.jpg",
-		  sort: 1
-		}, 
-		"江南小镇": {
-		  description : "没有什么能够阻挡",
-			thumbnail: "燕歌行.jpg",
-		  name: "南浔"
+	        "宇宙第一帅": {
+			description : "结婚前多拍拍自己的模样.",
+			thumbnail: "导师申请.png",
+			name: "本人"
 		},
 		"私密": {
 			description: "这是一个需要密码的相册",
-		  name: "私密",
-		  password: "233",
-			passwordTips: "密码是233"
+			name: "私密",
+			password: "159258",
+			passwordTips: "想要密码，就劳烦亲自来问"
+		},
+		"风景": {
+			description : "大千世界，多姿多彩。",
+			thumbnail: "雷雨3.jpg",
+			name: "美景"
 		}
-	}
+	}  
 }
